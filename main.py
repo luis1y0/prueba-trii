dd88ddff564aa4791c21a7b410e8069166f47cd4
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2022-02-03 17:58:57 -0600

    query total spend

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 import requests
 
 options = [
@@ -15,5 +16,14 @@ options = [
 
 api_url = 'https://financialmodelingprep.com/api/v3/quote-short/{}?apikey=c13a5d2ecf7cc6b8c50c06d7e1dfce22'
 
-response = requests.get(api_url.format(options[-1]))
-print(response.json())
+total_spend = 0
+for option in options:
+    response = requests.get(api_url.format(option))
+    stock_price_items = response.json()
+    try:
+        if len(stock_price_items) > 0 and type(stock_price_items) is list:
+            stock_price = stock_price_items[0]
+            total_spend += stock_price['price']
+    except:
+        print('Error')
+print('El costo de comprar todos los stock disponibles es {} USD'.format(total_spend))
